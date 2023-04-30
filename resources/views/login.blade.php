f1ab8ff9b6a8e1fef935af7dc81e18510f5840c4
Abdallah <aoubella88@gmail.com>
2023-04-30 11:16:49 +0100

    login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -12,9 +12,10 @@
 	alert('Email ou Mot de passe incorrect !');
  </script>
 @endif
-<!-- <header>
-		<p>connecter vous a votre espace travail</p>
-	</header> -->
+<p>Connectez-vous pour accéder <br> à votre espace !</p>
+<div class="cont">	
+
+<!--?xml version="1.0" standalone="no"?-->              <svg id="sw-js-blob-svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" version="1.1">                    <defs>                         <linearGradient id="sw-gradient" x1="0" x2="1" y1="1" y2="0">                            <stop id="stop1" stop-color="rgba(93, 92, 139, 1)" offset="0%"></stop>                            <stop id="stop2" stop-color="rgba(93, 92, 139, 1)" offset="100%"></stop>                        </linearGradient>                    </defs>                <path fill="url(#sw-gradient)" d="M27.9,-31.2C35,-27.3,38.7,-17.4,40.6,-7.1C42.5,3.2,42.4,13.8,37.6,21.1C32.7,28.4,22.9,32.2,13.1,35.7C3.3,39.1,-6.6,42.2,-14.6,39.5C-22.6,36.8,-28.7,28.5,-32,19.9C-35.3,11.3,-35.7,2.4,-34.9,-6.8C-34.1,-16.1,-32,-25.8,-25.9,-29.9C-19.9,-34,-9.9,-32.5,0.2,-32.8C10.4,-33.1,20.8,-35.2,27.9,-31.2Z" width="100%" height="100%" transform="translate(50 50)" stroke-width="0" style="transition: all 0.3s ease 0s;" stroke="url(#sw-gradient)"></path>              </svg>
 <div class="main">  
 	
 		<input type="checkbox" id="chk" aria-hidden="true">
@@ -33,7 +34,7 @@
 						 </div>
 					</div>
 					<input  type="submit" name="login" class="btn2" value="Login">
-					<h6>SFE © 2023</h6>
+					
 				</form>	
                 
 			</div>
@@ -52,5 +53,6 @@
 			</div>
 		 
 	</div>
-
+	</div>
+	<footer><h6>SFE © 2023</h6></footer>
 @endsection
